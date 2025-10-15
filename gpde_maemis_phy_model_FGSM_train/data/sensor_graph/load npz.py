6a19eb1db565b0c3413afbd01e05f3c528366585
Tianshu Bao <tianshubao11@gmail.com>
2025-10-15 15:11:19 -0500

    Add files via upload

---
+++ gpde_maemis_phy_model_FGSM_train/data/sensor_graph/load npz.py
@@ -0,0 +1,9 @@
+
+
+import numpy as np
+
+seed = np.load('METR_LA_rho_max.npy')
+# seed.files   # ['mis', 'width', 'mse', 'rmse', 'mae', 'prediction', 'truth']
+# temp = seed['prediction']
+# v = temp[0, :,:, :]     #[12, 3584, 307]
+# temp = seed['truth']
